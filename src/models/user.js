bcf6f5725d419555924693f09e261677f5ae319d
DerekChou <derekchou3704@gmail.com>
2021-06-07 01:21:11 +0800

    Authorization: storing hashed password and make email unique for independent users

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -10,6 +11,7 @@ const User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
+        unique: true,
         trim: true,
         lowercase: true,
         validate(val) {
@@ -33,5 +35,29 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+// Defining a method on the model
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+    if (!user) throw new Error('Unable to login')
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) throw new Error('Unable to login')
+
+    return user
+}
+
+// Hash the plain password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
